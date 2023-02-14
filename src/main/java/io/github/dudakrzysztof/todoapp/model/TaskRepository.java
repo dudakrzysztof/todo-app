7df75ba8b5975bf7416de840779a630ff9d4ffda
KrzysztofDuda <duda_krzysztof@o2.pl>
2023-02-14 17:42:52 +0100

    Initial commit

---
+++ src/main/java/io/github/dudakrzysztof/todoapp/model/TaskRepository.java
@@ -0,0 +1,8 @@
+package io.github.dudakrzysztof.todoapp.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+}
