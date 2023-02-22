298a4e381422fcaf37eab158689ef0079819ac88
KrzysztofDuda <duda_krzysztof@o2.pl>
2023-02-22 20:46:14 +0100

    RequestMapping added

---
+++ src/main/java/io/github/dudakrzysztof/todoapp/model/SqlTaskRepository.java
@@ -0,0 +1,10 @@
+package io.github.dudakrzysztof.todoapp.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+interface SqlTaskRepository extends TaskRepository, JpaRepository<Task, Integer> {
+
+}
--- src/main/java/io/github/dudakrzysztof/todoapp/model/TaskRepository.java
+++ src/main/java/io/github/dudakrzysztof/todoapp/model/TaskRepository.java
@@ -1,8 +1,20 @@
 package io.github.dudakrzysztof.todoapp.model;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 
-@RepositoryRestResource
-public interface TaskRepository extends JpaRepository<Task, Integer> {
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskRepository {
+    List<Task> findAll();
+    Page<Task> findAll(Pageable page);
+    Optional<Task> findById(Integer id);
+
+    boolean existsById(Integer id);
+
+    List<Task> findByDone(@Param("state") boolean done);
+    Task save(Task entity);
+
 }
